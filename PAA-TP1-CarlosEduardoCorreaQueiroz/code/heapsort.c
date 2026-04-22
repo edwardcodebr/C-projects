7018531efded4d594006023fba106c46bc034c5c
Carlos Eduardo Correa Queiroz <156525508+edwardcodebr@users.noreply.github.com>
2026-04-22 18:01:38 -0400

    Update heapsort.c

--- PAA-TP1-CarlosEduardoCorreaQueiroz/code/heapsort.c
+++ PAA-TP1-CarlosEduardoCorreaQueiroz/code/heapsort.c
@@ -1,1 +1,32 @@
-// implementação heapsort
+#include "sorts.h"
+
+void heapify(unsigned int arr[], int n, int i) {
+    int maior = i;
+    int esq = 2*i + 1;
+    int dir = 2*i + 2;
+
+    if (esq < n && arr[esq] > arr[maior])
+        maior = esq;
+
+    if (dir < n && arr[dir] > arr[maior])
+        maior = dir;
+
+    if (maior != i) {
+        unsigned int temp = arr[i];
+        arr[i] = arr[maior];
+        arr[maior] = temp;
+        heapify(arr, n, maior);
+    }
+}
+
+void heapSort(unsigned int arr[], int n) {
+    for (int i = n/2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n-1; i > 0; i--) {
+        unsigned int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        heapify(arr, i, 0);
+    }
+}
