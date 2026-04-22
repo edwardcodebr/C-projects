9428aa901fe36f101b8ea9668707b2aee52943c7
Carlos Eduardo Correa Queiroz <156525508+edwardcodebr@users.noreply.github.com>
2026-04-22 18:00:53 -0400

    Add functions to generate and copy arrays

--- PAA-TP1-CarlosEduardoCorreaQueiroz/code/utils.c
+++ PAA-TP1-CarlosEduardoCorreaQueiroz/code/utils.c
@@ -1,1 +1,15 @@
-// utils
+#include <stdlib.h>
+#include <time.h>
+#include "utils.h"
+
+void gerar_vetor(unsigned int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 65536;
+    }
+}
+
+void copiar_vetor(unsigned int origem[], unsigned int destino[], int n) {
+    for (int i = 0; i < n; i++) {
+        destino[i] = origem[i];
+    }
+}
