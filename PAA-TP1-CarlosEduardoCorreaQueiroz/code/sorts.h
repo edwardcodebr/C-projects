b7ef6119b907d35d2728fc858758d96c3aae0725
Carlos Eduardo Correa Queiroz <156525508+edwardcodebr@users.noreply.github.com>
2026-04-22 17:59:33 -0400

    Update sorts.h

--- PAA-TP1-CarlosEduardoCorreaQueiroz/code/sorts.h
+++ PAA-TP1-CarlosEduardoCorreaQueiroz/code/sorts.h
@@ -1,1 +1,8 @@
-// header sorts
+#ifndef SORTS_H
+#define SORTS_H
+
+void mergeSort(unsigned int arr[], int l, int r);
+void heapSort(unsigned int arr[], int n);
+void quickSort(unsigned int arr[], int low, int high);
+
+#endif
